ee9db90b0b755a6b64cbb3593a5cde7eceaffc66
DevVoxel <aidensmith20@gmail.com>
2023-08-06 17:28:57 -0400

    Light Shade Component

---
+++ components/LightShade.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+
+function LightShade(): JSX.Element {
+  return (
+    <>
+      <motion.svg
+        width='360'
+        height='360'
+        viewBox='0 0 2095 2490'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <motion.path
+          initial={{ pathLength: 0 }}
+          animate={{ pathLength: 1 }}
+          transition={{
+            duration: 2,
+            ease: 'easeInOut',
+            repeat: Infinity,
+            repeatType: 'loop',
+            repeatDelay: 1,
+          }}
+          strokeWidth={10}
+          strokeDasharray='0 1'
+          d='M3.5 1982.5L83 2084.5H421L489 2152.5H561L828 1811.5H976.5L1156 1617H1250L1368 1698V1540.5L1250 1418M1250 1418H1145M1250 1418H1383.5L1250 1252M1250 1252H1156M1250 1252H1318L1156 1059.5H1304.5L1492.5 840.5H1180M3.5 1986.5L348 1527.5V1366L263.5 1457.5V1359.5L311 1305.5H231.5L303.5 1233.5H438.5L650 1010M806.5 1846.5L977.5 1980V2091.5M977.5 2091.5V2093M977.5 2091.5L851 2254.5H937.5M1050.5 2018.5V2147L904.5 2348H1090.5M1123.5 2254.5V2352L1246.5 2480.5V2312M510.5 1158V932.5L715 714L703 233L1002 504.5M649.5 1202V934L1038 466.5V8C1136.5 96.3333 1333.5 275 1333.5 283V557.5M1148 743.5L1481.5 413.5V710.5L1350.5 841M1414 842.167L1599 646V871M1489.5 999.5L1687 764.5L1694.5 999.5L1578.5 1151M1559 1150.5H1624.5L1689 1069.5L1829.5 1065L1642.5 1297.5M1727 1193.5H1869L2026 1361H1727M1883 1362.5L2086 1578H1786.5L1639.5 1407M1787.5 1576L1769.5 1596H1855L1333.5 2168.5L1345 1967.5M1340.5 2022L1270 2110.5V1937L1362 1801.5M1317.5 1803L1236.5 1914.5V1803L1176 1744M808 1056H1030L1100 1142.5L976 1282V1180H831.5M9 1986H151L399 1660V1606.5L477 1522.5H416L487.5 1423.5H426.5L560.5 1244M498.5 1626.5L675 1408.5V1359.5M675 1359.5L704 1318.5M675 1359.5H596L509 1462M510 1459L523 1595M433.5 2096.5L723.5 1744H758.5L775 1721.03M775 1721.03L877 1579V1533L775 1668.5V1721.03ZM308 1836L273.75 1880.25M273.75 1880.25L239.5 1924.5V1973L274.5 1939.5L273.75 1880.25ZM1086.5 2053.5V2168M1174.5 1695V1793.5M1146 1689.5V1771.5'
+          stroke='#1CFFFB'
+        />
+        <g filter='url(#filter0_f_36_52)'>
+          <motion.path
+            d='M684.5 767C680.9 772.6 579.333 883.667 529 938.5V1105.5M815 824L732.5 920M825.5 869L782 920M1233.5 412V624.5M1378.5 701L1278 808.5M1048 982H1211.5M1286.5 1123H1438M1329 1164H1412.5M1443 1312C1448.6 1325.6 1514.67 1405 1547 1443M1438 1375C1439.2 1381.8 1539.5 1501.5 1589.5 1560.5H1511M1484 1558.5L1558.5 1662.5H1473.5M1434.5 1657.5L1529.5 1785M1495.5 1921V1986M1431 2067.5V1929.5L1472 1858V1786.5M1315.5 1710L1259 1780M1342.5 1739L1259 1854.5V1759.5L1201.5 1739M1293 2145.5V2256.5M1159 2181.5V2334.5M1016 1967V2135.5L937.5 2254.5M1058.5 1370L1159 1448H1242L1349.5 1555.5V1657.5M1094 1443L1269.5 1601M949.5 1727L988.5 1766.5M919 1747.5L968 1795M810 1802L549.5 2125.5H507L468 2093M88.5 2055.5H156.5M156.5 2055.5H400L461 1977H252L156.5 2055.5ZM434 1798.5L357.5 1882M423 1863L367 1922.5M1657.5 1496L1730.5 1600M1109.5 272V537M719.5 278.5L984.5 518M712.5 1354.5V1417L337.5 1866.5M763 1365.5V1457.5M1011.5 863.5H1196.5M275 1293.5L314.5 1251.5H445L632.5 1050.5M402.5 1309.5L352 1365.5M277.5 1426V1362.5L326 1308'
+            stroke='#FFFFFF'
+            initial={{ pathLength: 0 }}
+            animate={{ pathLength: 1 }}
+            transition={{
+              duration: 2,
+              ease: 'easeInOut',
+              repeat: Infinity,
+              repeatType: 'loop',
+              repeatDelay: 1,
+            }}
+            strokeWidth={10}
+            strokeDasharray='0 1'
+          />
+        </g>
+        <defs>
+          <filter
+            id='filter0_f_36_52'
+            x='0.5'
+            y='0'
+            width='1745.68'
+            height='2162.5'
+            filterUnits='userSpaceOnUse'
+            color-interpolation-filters='sRGB'
+          >
+            <feFlood flood-opacity='0' result='BackgroundImageFix' />
+            <feBlend
+              mode='normal'
+              in='SourceGraphic'
+              in2='BackgroundImageFix'
+              result='shape'
+            />
+            <feGaussianBlur
+              stdDeviation='15'
+              result='effect1_foregroundBlur_36_52'
+            />
+          </filter>
+        </defs>
+      </motion.svg>
+    </>
+  );
+}
+<svg
+  width='2024'
+  height='2314'
+  viewBox='0 0 2024 2314'
+  fill='none'
+  xmlns='http://www.w3.org/2000/svg'
+>
+  <g filter='url(#filter0_f_36_15)'>
+    <path
+      d='M15.5 1859.5H77.5L145 1778M119 1713H196L264.5 1630M1001.5 21.5L1252.5 254V534M1404 413.5V668.5L1273 801.5M1609 778.5V952.5L1489 1110.5M1733.5 1042L1578 1243H1505.5M1793.5 1168.5C1795.1 1176.5 1873.17 1262.83 1912 1305H1654.5M1816.5 1340.5L1988.5 1523H1729.5M1308.5 1793L1228.5 1903V2023.5M1374 1842.5L1303 1931.5L1295.5 2083M1007 1970V2118L875.5 2293M1523 650.5V818.5L1385.5 976.5M350.5 1352C351.7 1347.2 434.667 1239.33 476 1186L739 1195.5'
+      stroke='#002743'
+      stroke-width='17'
+    />
+  </g>
+  <defs>
+    <filter
+      id='filter0_f_36_15'
+      x='0.5'
+      y='0.26416'
+      width='2022.69'
+      height='2312.84'
+      filterUnits='userSpaceOnUse'
+      color-interpolation-filters='sRGB'
+    >
+      <feFlood flood-opacity='0' result='BackgroundImageFix' />
+      <feBlend
+        mode='normal'
+        in='SourceGraphic'
+        in2='BackgroundImageFix'
+        result='shape'
+      />
+      <feGaussianBlur
+        stdDeviation='7.5'
+        result='effect1_foregroundBlur_36_15'
+      />
+    </filter>
+  </defs>
+</svg>;
+
+export default LightShade;
